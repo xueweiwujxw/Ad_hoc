2515ad6d6d883899ec32a566b1c5a4a8a9e34824
xueweiwujxw <201700130033@mail.sdu.edu.cn>
2021-04-30 14:46:41 +0800

    🌈 style(trajectory_build): 使用pep8规范

--- trajectory_build.py
+++ trajectory_build.py
@@ -1,6 +1,7 @@
 import os
 import random
 
+
 def writeFile(nodeNum, posRes):
     filename = []
     for i in range(nodeNum):
@@ -16,23 +17,26 @@ def writeFile(nodeNum, posRes):
             f.write("Altitude_Method: absolute\n")
             f.write("locale: C\n")
             f.write("Coordinate_Count: " + str(len(posRes)) + "\n")
-            f.write("# X Position        Y Position          Altitude            Traverse Time       Wait Time           \n")
+            f.write(
+                "# X Position        Y Position          Altitude            Traverse Time       Wait Time           \n")
             for posItem in posRes[index]:
-                f.write("{:<20.9f},{:<20.9f},{:<20.9f},{:<20s},{:<20s}\n".format(posItem[0], posItem[1], posItem[2], posItem[3], posItem[4]))
+                f.write("{:<20.9f},{:<20.9f},{:<20.9f},{:<20s},{:<20s}\n".format(
+                    posItem[0], posItem[1], posItem[2], posItem[3], posItem[4]))
     print(filename)
+
 
 def calTrajectory(nodeNum, changeCount):
     posRes = []
     for i in range(nodeNum):
         pos = []
-        pos.append((0,0,0,"0h0m0.00s","0h0m0.00s"))
+        pos.append((0, 0, 0, "0h0m0.00s", "0h0m0.00s"))
         for j in range(changeCount):
             x = random.random() * 1000.0
             y = random.random() * 1000.0
             z = 0
-            x = x if random.randint(0,1) == 1 else -x
-            y = y if random.randint(0,1) == 1 else -y
-            pos.append((x,y,z,"0h0m10.00s","0h0m0.00s"))
+            x = x if random.randint(0, 1) == 1 else -x
+            y = y if random.randint(0, 1) == 1 else -y
+            pos.append((x, y, z, "0h0m10.00s", "0h0m0.00s"))
         posRes.append(pos)
     return posRes
 
